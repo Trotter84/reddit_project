dd33b99b8cd3225008fc8d818ec2f96c0bef250e
Daniel Trotter git config --globalgit config --global user.email flare84@gmail.comgit config --global user.email flare84@gmail.com <flare84@gmail.comgit config --global user.email flare84@gmail.comgit config --global user.name Daniel>
2018-09-04 10:27:36 -0600

    sub model, controller, and routes generated

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root "subs#index"
+
+  resources :subs
+  #resources :subs, only: [:new, :create, :edit, :update
+
+
+  # get "/subs", to: "subs#index"
+  # get "/sub/:id", to: "subs#show"
+  # delete "/subs/:id", to: "subs#destroy"
+
+end
