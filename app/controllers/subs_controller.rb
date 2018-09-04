c40dfecd2da47b6b6554ae08e0566605592b03e5
Daniel Trotter git config --globalgit config --global user.email flare84@gmail.comgit config --global user.email flare84@gmail.com <flare84@gmail.comgit config --global user.email flare84@gmail.comgit config --global user.name Daniel>
2018-09-04 14:23:39 -0600

    sub model, controller, and routes generated

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,22 +1,52 @@
 class SubsController < ApplicationController
   def index
+    @subs = Sub.all
+    # render :index
   end
 
   def show
   end
 
   def new
+    @sub = Sub.new
+    render partial: "form"
   end
 
   def create
+    @sub = Sub.new(sub_params)
+
+    if @sub.save
+      redirect_to subs_path
+    else
+      render :new
+    end
+  end
 
   def edit
+    render partial: "form"
   end
 
   def update
+
+    if @sub.update(sub_params)
+      # redirect_to @sub
+      redirect_to sub_path(@sub)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @sub.destroy
+    redirect_to subs_path
   end
 
+  private
+    def set_sub
+      @sub = Sub.find(params[:id])
+    end
+
+    def sub_params
+      params.require(:sub).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root "subs#index"
 
-  resources :subs
-  #resources :subs, only: [:new, :create, :edit, :update
+  resources :subs do
+    resources :topics
+  end
 
+
+  #resources :subs, only: [:new, :create, :edit, :update
 
   # get "/subs", to: "subs#index"
   # get "/sub/:id", to: "subs#show"
